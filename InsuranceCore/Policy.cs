b1129f8da87cb9d9f16476f5ff0461aad2b6f355
Jon Roberts <jroberts_consult@hotmail.com>
2026-09-24 10:27:43 -0500

    Add policy and test

---
+++ InsuranceCore/Policy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceCore
+{
+    public class Policy
+    {
+        public string policyId { get; set; }
+        public DateTimeOffset startDate { get; set; }
+        public DateTimeOffset endDate { get; set; }
+        public decimal deductible { get; set; }
+        public decimal coverageLimit { get; set; }
+        public List<string> coveredIncidents { get; set; }
+
+        public Policy(string policyId, DateTimeOffset startDate, DateTimeOffset endDate, decimal deductible, decimal coverageLimit, List<string> coveredIncidents)
+        {
+            this.policyId = policyId;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.deductible = deductible;
+            this.coverageLimit = coverageLimit;
+            this.coveredIncidents = coveredIncidents;
+
+        }
+    }
+}
---
+++ TestInsurance/PolicyTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InsuranceCore;
+using System;
+using System.Collections.Generic;
+
+namespace TestInsurance
+{
+    [TestClass]
+    class PolicyTests
+    {
+        [TestMethod]
+        [DataRow("1", "1/1/2000", "1/1/2000", 105.30, 105.30, "type1", "type2")]
+        public void CreatePolicy(string policyId, string startDateString, string endDateString, double deductibleDouble, double coverageLimitDouble, params string[] coveredIncidentsArray)
+        {
+            DateTimeOffset startDate = DateTimeOffset.Parse(startDateString);
+            DateTimeOffset endDate = DateTimeOffset.Parse(endDateString);
+            decimal deductible = (decimal)deductibleDouble;
+            decimal coverageLimit = (decimal)coverageLimitDouble;
+            List<string> coveredIncidents = new List<string>(coveredIncidentsArray);
+
+            Policy policy = new Policy(policyId, startDate, endDate, deductible, coverageLimit, coveredIncidents);
+            Assert.AreEqual(policyId, policy.policyId);
+            Assert.AreEqual(startDate, policy.startDate);
+            Assert.AreEqual(endDate, policy.endDate);
+            Assert.AreEqual(deductible, policy.deductible);
+            Assert.AreEqual(coverageLimit, policy.coverageLimit);
+            Assert.AreEqual(coveredIncidents, policy.coveredIncidents);
+
+        }
+    }
+}
